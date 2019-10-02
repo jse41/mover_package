c35224e6b11afa946348c63cb69657a0e50f05d3
jse41 <jse41@case.edu>
2019-10-02 15:58:53 -0400

    Starts competition and recieves orders. Also depends on osrf gear

--- src/mover_code.cpp
+++ src/mover_code.cpp
@@ -3,6 +3,19 @@
 
 #include <sstream>
 
+#include "std_srvs/Trigger.h"
+#include "osrf_gear/Order.h"
+
+
+std::vector<osrf_gear::Order> order_vector; 
+
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void orderCallback(const osrf_gear::Order& msg)
+{
+  ROS_INFO("I heard: [%s]", msg.order_id.c_str());
+}
 
 int main(int argc, char **argv)
 {
@@ -16,7 +29,7 @@ int main(int argc, char **argv)
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "mover");
 
   /**
    * NodeHandle is the main access point to communications with the ROS system.
@@ -25,7 +38,18 @@ int main(int argc, char **argv)
    */
   ros::NodeHandle n;
 
+  std_srvs::Trigger begin_comp;
+
+  ros::ServiceClient begin_client = n.serviceClient<std_srvs::Trigger>("/ariac/start_competition"); 
+
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+  ros::Subscriber sub = n.subscribe("/ariac/orders", 1000, orderCallback);
+
+  // Start Competition
+  begin_client.call(begin_comp);
+
+  ROS_INFO("Competition service returned: %s", begin_comp.response.message.c_str());
 
   ros::Rate loop_rate(10);
 
